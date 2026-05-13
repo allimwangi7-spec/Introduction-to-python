57b51ab86d4450fcc4689a5a90eb2f8a8aebab19
allimwangi7-spec <allimwangi7@gmail.com>
2026-05-13 14:55:49 +0300

    my name,age and course

---
+++ comments.py
@@ -0,0 +1,16 @@
+#there are 2 ways of writing comments
+#1 using hash symbol
+#2 using tripple quotes
+#importance of comments
+#-to explain what the program is doing
+#-to explain what part of code is doing
+#-to put some kind of meaning to your code
+"""
+a programe to display my
+name-
+age-
+course-
+
+"""
+
+print("my name is Allen. I am 20 years of age. I am doing program couse")
