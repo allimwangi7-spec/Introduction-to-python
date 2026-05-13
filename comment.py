5bd826c598286b988b03627bf9b31b825eb6d8d4
allimwangi7-spec <allimwangi7@gmail.com>
2026-05-13 15:12:55 +0300

    why I am learnig python

---
+++ comment.py
@@ -0,0 +1,10 @@
+"""
+importance of python in tech world
+gateway to AI and machine learning
+Automation and scripting
+Rapid prototyping and web developoment
+Scientific computing and education
+
+"""
+
+print("I am learning python because I want to be a backend developer")
