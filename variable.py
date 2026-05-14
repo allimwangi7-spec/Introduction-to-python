91612ae01c3167f90585a7a2f4833c0dd8c5badd
allimwangi7-spec <allimwangi7@gmail.com>
2026-05-14 13:10:37 +0300

    name, product bought and its price

---
+++ variable.py
@@ -0,0 +1,15 @@
+"""
+
+variables-
+A variable is a container that holds information about something
+When naming variables use 
+-Meaningful names such as student names
+-Do not use wide spaces
+-Do not use special symbols
+-a program to create a variable called name, product bought, and its price and display the results in console window
+
+"""
+name="Timothy Onyango"
+product_bought="Iphone 14"
+price =100000
+print(f"customer name {name},product bought {product_bought}, Its price is {price}")
