f15bcd36bd0b69afcba6b626407787a2fb35f81d
allimwangi7-spec <allimwangi7@gmail.com>
2026-05-15 13:55:23 +0300

    Definition of datatypes

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+"""
+
+Datatypes refers to type of value that tells a computer what to store.
+Python supports the following datatypes:
+(1) Integers: store whole number values
+(2)String: Stores string values or group og characters
+(3)Float: Stores a with a decimal point partr
+(4)Boolean: Returns true/false
+(5)List: List of values
+(6)Dictionary 
+
+"""
+
+name="Juma James"
+age=30
+has_completed=True
+fees_paid=750000.00
+topics=['Introduction', 'Input', 'Variables', 'Datatype']
+
+print(type(name))
+print(type(age))
+print(type(has_completed))
+print(type(fees_paid))
+print(type(topics))
