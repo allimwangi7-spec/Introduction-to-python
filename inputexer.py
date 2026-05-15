9da428d5219940791a36b01e64939c8bae57719a
allimwangi7-spec <allimwangi7@gmail.com>
2026-05-15 12:41:16 +0300

    Simple python script

---
+++ inputexer.py
@@ -0,0 +1,7 @@
+"""
+
+A simple python script that asks user input his/her name, the school he/she is joining,
+the course and the coure cause, output the name, school, course from user input on 
+console windows
+
+"""
